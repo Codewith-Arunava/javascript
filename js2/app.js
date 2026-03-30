19458e9e7b0712132e382e4a414e165afd681724
Arunava Chakraborty <144585145+Arunava-chakrabor@users.noreply.github.com>
2026-03-30 23:08:27 +0530

    js

--- js2/app.js
+++ js2/app.js
@@ -1,14 +1,49 @@
-console.log("Hello World!");
+// console.log("Hello World!");
 
-let x=666
-console.log(x);
-console.log(typeof(x));
+// let x=666
+// console.log(x);
+// console.log(typeof(x));
 
-function createcourse(coursename) {
-    console.log(coursename);
+// function createcourse(coursename) {
+//     console.log(coursename);
+// }
+
+// createcourse("Angular");
+// createcourse("React");
+
+
+//scope
+//global scope
+
+// let a=10;
+// function test() {
+//     console.log(a);
+// }
+// test();
+// console.log(a);
+
+let a = 1;
+
+function demo() {
+  let b = 2;
+
+  if (true) {
+    let c = 3;
+    console.log(a, b, c);
+  }
+}
+demo();
+
+function one() {
+  two();
 }
 
-createcourse("Angular");
-createcourse("React");
-document.getElementById("demo").innerHTML = "Hello JavaScript!";
-debugger
+function two() {
+  three();
+}
+
+function three() {
+  console.log("Done");
+}
+
+one();
